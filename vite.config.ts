549237a6a96e956043ffcb1a39e6f7bcc36a7d28
Karolis <karolis.vilpisauskas@mediapark.com>
2022-09-24 22:36:00 +0300

    feat: Setup deployments

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://karolis-vilpisauskas.github.io/car-control-v2",
+  base: "https://karolis-vilpisauskas.github.io/car-control-v2/",
   plugins: [react()],
 });
