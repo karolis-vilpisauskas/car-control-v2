50109fd5b80fb30704d8ab574adde08147ffb2ba
Karolis <karolis.vilpisauskas@mediapark.com>
2022-12-03 14:55:59 +0200

    feat: Updated mobile api url

--- src/state/Mode.ts
+++ src/state/Mode.ts
@@ -1,7 +1,7 @@
 import create from "zustand";
 
 export const windowsApiUrl = "192.168.0.150";
-export const onePlusApiUrl = "192.168.45.111";
+export const onePlusApiUrl = "192.168.234.96";
 
 export type Mode = "desktop" | "mobile";
 
